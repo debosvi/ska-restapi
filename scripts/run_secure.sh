4c13e159421fe3a5cef0c27239bc0ae06aa4b8ce
Vincent de RIBOU <v.deribou@gmail.com>
2020-08-27 18:06:18 +0200

    Add certificates vars to TLS script

--- scripts/run_secure.sh
+++ scripts/run_secure.sh
@@ -5,4 +5,8 @@ THISDIR=$(pwd)
 
 PATH=$PWD/build-deps/target/bin:$PATH
 
+export KEYFILE=$THISDIR/aquaplouf.mooo.com.key
+export CERTFILE=$THISDIR/aquaplouf.mooo.com.bundle.crt
+export CAFILE=$THISDIR/ca.crt
+
 s6-tlsserver -- 127.0.0.1 4444 ./build/.built/bin/httpd
